edf866c25c8a021d63fbd10e4daeb81916f4feb4
hmwebdev <non_phixion418@hotmail.com>
2024-02-15 11:46:12 -0500

    Validation for the choice of player

--- app/app.js
+++ app/app.js
@@ -8,25 +8,25 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   let result;
   if (
-    playerSelection.toLowerCase() === "rock" &&
-    computerSelection === "paper"
-  ) {
-    result = "You Lose! Paper beats Rock";
-  } else if (
     playerSelection.toLowerCase() === "paper" &&
-    computerSelection == "scissor"
+    computerSelection === "rock"
   ) {
-    result = "You Lose! Scissor beats Paper";
+    result = "You Win! Paper beats Rock";
   } else if (
     playerSelection.toLowerCase() === "scissor" &&
-    computerSelection == "rock"
+    computerSelection == "paper"
   ) {
-    result = "You Lose! Rock beats scissor";
+    result = "You Win! Scissor beats Paper";
+  } else if (
+    playerSelection.toLowerCase() === "rock" &&
+    computerSelection == "scissor"
+  ) {
+    result = "You Win! Rock beats scissor";
   } else if (playerSelection.toLowerCase() === computerSelection) {
-    result = "Equal! Play again";
+    result = "Equal!";
   } else {
-    result = `You win! ${playerSelection} beats ${
-      computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+    result = `You Lost! ${computerSelection} beats ${
+      playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
     }`;
   }
   return result;
@@ -35,8 +35,12 @@ function playRound(playerSelection, computerSelection) {
 function playGame() {
   for (let i = 1; i <= 5; i++) {
     let choice = prompt("Choose your weapon!");
-    let roundScore = playRound(choice, getComputerChoice());
-    document.getElementById("score" + i).innerHTML = roundScore;
+    if (choice === "rock" || choice === "paper" || choice === "scissor") {
+      let roundScore = playRound(choice, getComputerChoice());
+      document.getElementById("score" + i).innerHTML = roundScore;
+    } else {
+      alert("Please enter a valid value (rock, paper, scissor");
+    }
   }
 }
 
